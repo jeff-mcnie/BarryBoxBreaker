d312a0d2d19781455bb3bbe42fe5b4ac7a1a2e6e
Jeff McNie <jeff.mcnie@gmail.com>
2022-07-23 18:04:49 +1200

    Add character image, random move functionality

--- game.js
+++ game.js
@@ -1,3 +1,30 @@
 // I want the character to move based on arrow inputs
+// Set event listner for key presses
 
+document.addEventListener('keyup', moveCharacter)
 
+// Set up character
+
+let character = document.createElement('img')
+character.setAttribute("src", "./images/character.jpeg")
+character.setAttribute("class", "character")
+
+// Set starting position of character on screen
+
+document.getElementById('start').appendChild(character)
+
+// Set parameters of where character could move
+
+let moveArea = document.getElementsByTagName('td')
+
+// for fun, make him move randomly based on a key press
+// It's so much fun, but kind of useless
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+function moveCharacter() {
+  let newArea = moveArea[getRandomNumber(0, moveArea.length)]
+  newArea.appendChild(character)
+}
